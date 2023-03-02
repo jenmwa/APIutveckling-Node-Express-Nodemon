c9e5f563e34f2a586c054c6f5949686c28dd99b2
jenmwa <jenmwa@gmail.com>
2023-03-02 22:20:23 +0100

    createUser+passsword to server

--- createLogin/login-back/app.js
+++ createLogin/login-back/app.js
@@ -3,11 +3,18 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+const cors = require('cors')
+
+
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const loginRouter = require('./routes/login');
 
-var app = express();
+const app = express();
+
+
+
+app.use(cors())
 
 app.use(logger('dev'));
 app.use(express.json());
--- createLogin/login-back/routes/users.js
+++ createLogin/login-back/routes/users.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
+//object id ist för hårdkodade id
+
 let users = [
   {userId: 1, userName: 'Knatte', password: 'hej'},
   {userId: 2, userName: 'Fnatte', password: 'hopp'},
-  {userId: 3, userName: 'Tjatte', password: 'hallo'}
+  {userId: 3, userName: 'Tjatte', password: 'hallo'},
+  {userId: 4, userName: 'Kalle', password: 'hi'}
   ];
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  res.json(users);
+});
+
+router.post('/', function(req, res, next) {
+  let newUser = req.body;
+  newUser.userId = users.length + 1;
+  users.push(newUser);
+
+  console.log(newUser)
+
   res.json(users);
 });
 
