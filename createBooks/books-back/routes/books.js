7dea6b95afc6c39660d5750438eb74e7f92b3f53
jenmwa <jenmwa@gmail.com>
2023-03-06 09:53:45 +0100

    added id jso

--- createBooks/books-back/routes/books.js
+++ createBooks/books-back/routes/books.js
@@ -1,22 +1,98 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-    let books = [
-        {id: 1, name: 'Don Quixote', author: 'Miguel de Cervantes', pages: 109, available: true},
-        {id: 2, name: 'Lord of the Rings', author: 'J.R.R. Tolkien', pages: 1209, available: true},
-        {id: 3, name: 'Harry Potter and the Sorcerer´s Stone', author: 'J.K. Rowling', pages: 324, available: false},
-        {id: 4, name: 'And Then There Were None', author: 'Agatha Christie', pages: 258, available: true},
-        {id: 5, name: 'Alice´s Adventures in Wonderland', author: ' Lewis Carroll', pages: 78, available: true},
-        {id: 6, name: 'The Lion, the Witch, and the Wardrobe', author: 'C.S. Lewis', pages: 108,available: true},
-        {id: 7, name: 'Pinocchio', author: ' Carlo Collodi', pages: 49, available: true},
-        {id: 8, name: 'Catcher in the Rye', author: ' J.D. Salinger', pages: 116, available: false},
-        {id: 9, name: 'Anne of Green Gables', author: 'L. M. Montgomery', pages: 382, available: true},
-        {id: 10, name: 'Twenty Thousand Leagues Under the Sea', author: 'Jules Verne', pages: 259, available: false}
-    
-    ];
-    res.json(books);
+
+  let books = [
+    {
+      id: 1,
+      name: "Don Quixote",
+      author: "Miguel de Cervantes",
+      pages: 109,
+      available: true,
+    },
+    {
+      id: 2,
+      name: "Lord of the Rings",
+      author: "J.R.R. Tolkien",
+      pages: 1209,
+      available: true,
+    },
+    {
+      id: 3,
+      name: "Harry Potter and the Sorcerer´s Stone",
+      author: "J.K. Rowling",
+      pages: 324,
+      available: false,
+    },
+    {
+      id: 4,
+      name: "And Then There Were None",
+      author: "Agatha Christie",
+      pages: 258,
+      available: true,
+    },
+    {
+      id: 5,
+      name: "Alice´s Adventures in Wonderland",
+      author: " Lewis Carroll",
+      pages: 78,
+      available: true,
+    },
+    {
+      id: 6,
+      name: "The Lion, the Witch, and the Wardrobe",
+      author: "C.S. Lewis",
+      pages: 108,
+      available: true,
+    },
+    {
+      id: 7,
+      name: "Pinocchio",
+      author: " Carlo Collodi",
+      pages: 49,
+      available: true,
+    },
+    {
+      id: 8,
+      name: "Catcher in the Rye",
+      author: " J.D. Salinger",
+      pages: 116,
+      available: false,
+    },
+    {
+      id: 9,
+      name: "Anne of Green Gables",
+      author: "L. M. Montgomery",
+      pages: 382,
+      available: true,
+    },
+    {
+      id: 10,
+      name: "Twenty Thousand Leagues Under the Sea",
+      author: "Jules Verne",
+      pages: 259,
+      available: false,
+    },
+  ];
+
+router.get("/", function (req, res, next) {
+  res.json(books);
+});
+
+router.get("/:id", function (req, res) {
+  let id = Number(req.params.id);
+  let bookId = books.find((book) => book.id === id);
+
+  if (bookId) {
+    res.json({
+      name: bookId.name,
+      title: bookId.author,
+      pages: bookId.pages,
+      available: bookId.available
+    });
+  } else {
+    res.status(401).json("Book not found");
+  }
 });
 
 module.exports = router;
