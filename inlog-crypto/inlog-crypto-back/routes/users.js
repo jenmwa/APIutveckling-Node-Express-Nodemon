0aad5e1c2b6519cbbbd035511af102daafc0a412
jenmwa <jenmwa@gmail.com>
2023-03-10 18:43:23 +0100

    array cleanup

--- inlog-crypto/inlog-crypto-back/routes/users.js
+++ inlog-crypto/inlog-crypto-back/routes/users.js
@@ -20,10 +20,41 @@ router.get('/', function(req, res, next) {
 
 });
 
-/* GET users listing. */
-router.get('/add', function(req, res, next) {
-  res.send('respond with a resource!!!!!!!');
+/* READ user list jsonFile. */
+router.get('/add', function(request, response, next) {
+  fs.readFile('users.json', function(error, data) {
+    if(error){
+      console.log(error)
+    }
+    const userList = JSON.parse(data);
+    response.send(userList);
+  })
 });
+
+router.post('/add', function(request, response, next) {
+  fs.readFile('users.json', function(error, data) {
+    if(error) {
+      console.log(error)
+    }
+
+    const userList = JSON.parse(data);
+    let newUser = {
+      'userName': request.body.userName,
+      'userPassword': request.body.userPassword,
+    };
+
+    userList.push(newUser);
+
+    fs.writeFile('users.json', JSON.stringify(userList, null, 2), function(error) {
+      if(error) {
+        console.log('Something went wrong!')
+      }
+    })
+    response.send(userList);
+    return;
+
+  })
+})
 
 
 module.exports = router;
