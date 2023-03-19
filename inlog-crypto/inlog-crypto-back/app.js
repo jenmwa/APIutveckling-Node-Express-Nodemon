3439edd2cb2e03687dd57817126f85b7b6b8b981
jenmwa <jenmwa@gmail.com>
2023-03-19 22:51:06 +0100

    mongodb added

--- inlog-crypto/inlog-crypto-back/app.js
+++ inlog-crypto/inlog-crypto-back/app.js
@@ -1,24 +1,34 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-const cors = require('cors');
+const cors = require("cors");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+
+const MongoClient = require("mongodb").MongoClient;
 
 var app = express();
 
 app.use(cors());
 
-app.use(logger('dev'));
+MongoClient.connect('mongodb://localhost:27017')
+  .then(client => {
+    console.log('we´re connected to the database');
+    const db = client.db('users');
+    app.locals.db = db;
+  })
+  .catch(err => console.log('err', err))
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 module.exports = app;
